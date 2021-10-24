1bbf9bf3446e76a4f0d7b41b276ae1df9d6e2a67
minho <maxkmh712@gmail.com>
2021-10-24 16:25:48 +0900

    [CREATED] users, cores, posts app & users modeling

--- wanted_minhokim/settings.py
+++ wanted_minhokim/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-from my_settings import DATABASES, SECRET_KEY
+from my_settings import SECRET_KEY
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
+    'users',
+    'posts',
+    'core',
 ]
 
 MIDDLEWARE = [
@@ -77,7 +80,12 @@ WSGI_APPLICATION = 'wanted_minhokim.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = DATABASES
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
